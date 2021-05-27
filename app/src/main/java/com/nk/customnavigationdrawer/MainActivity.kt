ab41c4e57cf499a70227921e062e37609a801c1c
n-k-d <n.k.d.assist@gmail.com>
2021-05-27 07:45:05 +0530

    First tut

--- app/src/main/java/com/nk/customnavigationdrawer/MainActivity.kt
+++ app/src/main/java/com/nk/customnavigationdrawer/MainActivity.kt
@@ -2,10 +2,35 @@ package com.nk.customnavigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var navController: NavController
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
     }
 }
